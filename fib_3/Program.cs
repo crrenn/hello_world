6cff2ea870c92165d4b03165891d0d2970acbc92
crrenn <rennc@ethz.ch>
2017-02-12 12:57:03 +0100

    fib 3

---
+++ fib_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i =0;
+            int j = 0;
+            for(int counter=0; counter <100; counter++){
+                i = i+1;
+                j = j-1;
+
+            }
+            Console.WriteLine(i);
+            Console.WriteLine("\n\n"+ j);
+        }
+    }
+}
